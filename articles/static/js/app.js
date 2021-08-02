400abd595cd5a217bcef37fd1c30a0d51137022f
sharansukesh1003 <sharansukesh2000@gmail.com>
2021-08-03 00:05:13 +0530

    fix loader bug, order by date and time, add the end in the end of posts, add styling for carousel

--- articles/static/js/app.js
+++ articles/static/js/app.js
@@ -2,9 +2,9 @@ var infinite = new Waypoint.Infinite({
     element: $('.infinite-container')[0],
     offset: 'bottom-in-view',
     onBeforePageLoad: function(){
-      
+      $('.loader').show();
     },
     onAfterPageLoad: function(){
-      
+     $('.loader').hide();
     },
   })
